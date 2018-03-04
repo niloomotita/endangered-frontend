d10de1c608bba6889d155ae0ad7b8c13c58539bd
niloofarmotamed <niloofar.motamed@gmail.com>
2018-03-04 23:21:24 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h3>Endangered species</h3>
+        </header>
+        <section className="App-body">
+          <h4>
+            This is a quick and easy to use service to place pictures of
+            endangered species as placeholder in your design or code. The goal of this 
+            service is to bring consciousness towards these animals.
+          </h4>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
