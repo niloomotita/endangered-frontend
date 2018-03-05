8304d0ca5ddd12c070f77273edfe95915039e6c4
niloofarmotamed <niloofar.motamed@gmail.com>
2018-03-05 13:28:30 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
-import redPanda from "./img/RedPanda.jpg"; 
+import redPanda from "./img/RedPanda.jpg";
 import pangolin from "./img/pangolin.jpg";
 import africanPenguin from "./img/African-penguins.jpg";
 import lowlandGorilla from "./img/Eastern-lowland-gorilla.jpg";
-import greenSeaTurtle  from "./img/green-sea-turtle.jpg";
+import greenSeaTurtle from "./img/green-sea-turtle.jpg";
 import yellowHeadedAmazon from "./img/yellow-headed.jpg";
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
         <section className="App-body">
           <div className="text">
             <h2 className="title">Endangered Placeholders</h2>
@@ -22,125 +21,152 @@ class App extends Component {
               our URL.
             </p>
             <p className="example">
-              Like this: http://endangered.io/200/400 <br/><br/>
+              Like this: http://endangered.io/200/400 <br />
+              <br />
               example: <pre>{`<img src="http://endangered.io/200/400">`}</pre>
             </p>
-            
           </div>
           <div className="imgs-clmn">
-           <div className=" animal">
-             <img src={pangolin} alt="" className="pangolin"/>
-             <span className="description">
-               <p className="animal-title">Pangolin</p>
-               <p> Critically Endangered</p>
-              <div className="links">
-                <a 
-                  href=" https://www.flickr.com/photos/string_bass_dave/" 
-                  className="link">Photo by David Brossard
-                </a> 
-                <span className="space">/</span>
-                <a 
-                  href="https://creativecommons.org/licenses/by/2.0/" 
-                  className="link">CC BY 2.0
-                  </a> 
-              </div>
-             </span>
-           </div>
-           <div className=" animal">
-            <img src={redPanda} alt="" className="redpanda"/>
-             <span className="description">
-                <p className="animal-title">Red Panda</p>
-                <p> Endangered <br/> Population less than 10,000 individuals</p>
+            <div className=" animal">
+              <img src={pangolin} alt="" className="pangolin" />
+              <span className="description">
+                <p className="animal-title">Pangolin</p>
+                <p> Critically Endangered</p>
                 <div className="links">
-                <a 
-                  href=" https://creativecommons.org/licenses/by-sa/3.0/" 
-                  className="link"> Photo by Greg Hume
-                </a> 
-                <span className="space">/</span>
-                <a 
-                  href="https://creativecommons.org/licenses/by-sa/3.0/" 
-                  className="link">CC BY-SA 3.0
-                  </a> 
-              </div>
-             </span>
-           </div>
-            <div className="animal">
-            <img src={africanPenguin} alt="" className="african-penguin"/>
-             <span className="description">
-                <p className="animal-title">African Penguin</p>
-               <p> Endangered</p>
-               <div className="links">
-                <a 
-                  href=" www.sharpphotography.co.uk" 
-                  className="link"> Photo by Charles J Sharp
-                </a> 
-                <span className="space">/</span>
-                <a 
-                  href=" https://creativecommons.org/licenses/by-sa/3.0/" 
-                  className="link">CC BY-SA 3.0
-                  </a> 
-              </div>
-             </span>
+                  <a
+                    href=" https://www.flickr.com/photos/string_bass_dave/"
+                    className="link"
+                  >
+                    Photo by David Brossard
+                  </a>
+                  <span className="space">/</span>
+                  <a
+                    href="https://creativecommons.org/licenses/by/2.0/"
+                    className="link"
+                  >
+                    CC BY 2.0
+                  </a>
+                </div>
+              </span>
             </div>
-          
+            <div className=" animal">
+              <img src={redPanda} alt="" className="redpanda" />
+              <span className="description">
+                <p className="animal-title">Red Panda</p>
+                <p>
+                  {" "}
+                  Endangered <br /> Population less than 10,000 individuals
+                </p>
+                <div className="links">
+                  <a
+                    href=" https://creativecommons.org/licenses/by-sa/3.0/"
+                    className="link"
+                  >
+                    {" "}
+                    Photo by Greg Hume
+                  </a>
+                  <span className="space">/</span>
+                  <a
+                    href="https://creativecommons.org/licenses/by-sa/3.0/"
+                    className="link"
+                  >
+                    CC BY-SA 3.0
+                  </a>
+                </div>
+              </span>
+            </div>
+            <div className="animal">
+              <img src={africanPenguin} alt="" className="african-penguin" />
+              <span className="description">
+                <p className="animal-title">African Penguin</p>
+                <p> Endangered</p>
+                <div className="links">
+                  <a href=" www.sharpphotography.co.uk" className="link">
+                    {" "}
+                    Photo by Charles J Sharp
+                  </a>
+                  <span className="space">/</span>
+                  <a
+                    href=" https://creativecommons.org/licenses/by-sa/3.0/"
+                    className="link"
+                  >
+                    CC BY-SA 3.0
+                  </a>
+                </div>
+              </span>
+            </div>
           </div>
           <div className="imgs-clmn">
             <div className="animal">
-              <img src={lowlandGorilla} alt="" className="lowland-gorilla"/>
+              <img src={lowlandGorilla} alt="" className="lowland-gorilla" />
               <span className="description">
-              <p className="animal-title">Eastern Lowland Gorilla</p>
-               <p> Critically Endangered</p>
-               <div className="links">
-                <a 
-                  href="https://www.flickr.com/people/72063229@N00" 
-                  className="link">Photo by Joe McKenna
-                </a> 
-                <span className="space">/</span>
-                <a 
-                  href="https://creativecommons.org/licenses/by/2.0/" 
-                  className="link">CC BY 2.0
-                  </a> 
-              </div>
+                <p className="animal-title">Eastern Lowland Gorilla</p>
+                <p> Critically Endangered</p>
+                <div className="links">
+                  <a
+                    href="https://www.flickr.com/people/72063229@N00"
+                    className="link"
+                  >
+                    Photo by Joe McKenna
+                  </a>
+                  <span className="space">/</span>
+                  <a
+                    href="https://creativecommons.org/licenses/by/2.0/"
+                    className="link"
+                  >
+                    CC BY 2.0
+                  </a>
+                </div>
               </span>
             </div>
             <div className="animal">
-              <img src={greenSeaTurtle} alt="" className="green-sea-turtle"/>
+              <img src={greenSeaTurtle} alt="" className="green-sea-turtle" />
               <span className="description">
-              <p className="animal-title">Green Turtle</p>
-               <p> Endangered</p>
-               <div className="links">
-                <a 
-                  href=" https://sites.google.com/site/thebrockeninglory/" 
-                  className="link">Photo by Brocken Inaglory
-                </a> 
-                <span className="space">/</span>
-                <a 
-                  href=" http://www.gnu.org/copyleft/fdl.html" 
-                  className="link">GFDL
-                  </a> 
-              </div>
+                <p className="animal-title">Green Turtle</p>
+                <p> Endangered</p>
+                <div className="links">
+                  <a
+                    href=" https://sites.google.com/site/thebrockeninglory/"
+                    className="link"
+                  >
+                    Photo by Brocken Inaglory
+                  </a>
+                  <span className="space">/</span>
+                  <a
+                    href=" http://www.gnu.org/copyleft/fdl.html"
+                    className="link"
+                  >
+                    GFDL
+                  </a>
+                </div>
               </span>
             </div>
             <div className="animal">
-              <img src={yellowHeadedAmazon} alt="" className="yellow-headed-amazon"/>
+              <img
+                src={yellowHeadedAmazon}
+                alt=""
+                className="yellow-headed-amazon"
+              />
               <span className="description">
-              <p className="animal-title">Yellow Headed Amazon</p>
-               <p>  Endangered</p>
-               <div className="links">
-                <a 
-                  href=" https://www.flickr.com/photos/kurt-b/" 
-                  className="link">Photo by Kurt Bauschardt
-                </a> 
-                <span className="space">/</span>
-                <a 
-                  href="https://creativecommons.org/licenses/by/2.0/" 
-                  className="link">CC BY 2.0
-                  </a> 
-              </div>
+                <p className="animal-title">Yellow Headed Amazon</p>
+                <p> Endangered</p>
+                <div className="links">
+                  <a
+                    href=" https://www.flickr.com/photos/kurt-b/"
+                    className="link"
+                  >
+                    Photo by Kurt Bauschardt
+                  </a>
+                  <span className="space">/</span>
+                  <a
+                    href="https://creativecommons.org/licenses/by/2.0/"
+                    className="link"
+                  >
+                    CC BY 2.0
+                  </a>
+                </div>
               </span>
             </div>
-            
-           
           </div>
         </section>
       </div>
